4e61e929a63b77bf44c1d806d421b7ac1d8cb56c
andresgutierrezv <andres.gutierrez.v@koneta-group.com>
2024-04-17 00:06:49 -0500

    se agrego api

---
+++ src/app.ts
@@ -0,0 +1,39 @@
+import { BaileysProvider, handleCtx } from '@bot-whatsapp/provider-baileys'
+import {
+  MemoryDB,
+  addKeyword,
+  createBot,
+  createFlow,
+  createProvider
+} from '@bot-whatsapp/bot'
+
+const flowBienvenida = addKeyword('hola').addAnswer('buenas! bienvenido')
+
+const main = async () => {
+  const provider = createProvider(BaileysProvider)
+
+  await createBot({
+    flow: createFlow([flowBienvenida]),
+    database: new MemoryDB(),
+    provider
+  })
+
+  provider.initHttpServer(3002)
+
+  provider.http.server.post(
+    '/send-message',
+    handleCtx(async (bot, req, res) => {
+      const body = req.body
+      const message = body.message
+      const phone = body.phone
+
+      // await bot.sendMessage('573217442971', 'pepe hola hola', {})
+
+      await bot.sendMessage(phone, message, {})
+
+      res.end('se envio')
+    })
+  )
+}
+
+main()
