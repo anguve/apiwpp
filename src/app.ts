a71098b388b3e12953616942120cfaddee2fc99e
andresgutierrezv <andres.gutierrez.v@koneta-group.com>
2024-04-20 12:46:42 -0500

    se refactorizo y sirve para cualquier nuermo3

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,11 @@ import {
   createProvider
 } from '@bot-whatsapp/bot'
 
-// Constants
-const PORT_DEFAULT = 3000
-
-// Functions
-const initBotServer = async () => {
-  const flowBienvenida = addKeyword('hola').addAnswer('buenas! bienvenido')
-  const port: number = parseInt(process.env.PORT || String(PORT_DEFAULT), 10)
-
+const flowBienvenida = addKeyword('hola').addAnswer('buenas! bienvenido')
+const port: number = parseInt(process.env.PORT || '3000', 10)
+const main = async () => {
   const provider = createProvider(BaileysProvider)
+
   await createBot({
     flow: createFlow([flowBienvenida]),
     database: new MemoryDB(),
@@ -23,29 +19,17 @@ const initBotServer = async () => {
   })
 
   provider.initHttpServer(port)
-  setupMessageEndpoint(provider)
-}
 
-const setupMessageEndpoint = (provider: any) => {
   provider.http.server.post(
     '/send-message',
     handleCtx(async (bot, req, res) => {
-      const { message, phone } = req.body
-
+      const body = req.body
+      const message = body.message
+      const phone = body.phone
       await bot.sendMessage(String(phone), message, {})
-      res.end('se envió el mensaje.')
+      res.end('se envio mensaje')
     })
   )
 }
 
-// Main function
-const main = async () => {
-  try {
-    await initBotServer()
-  } catch (error) {
-    console.error('Error initializing bot:', error)
-  }
-}
-
-// Run main function
 main()
